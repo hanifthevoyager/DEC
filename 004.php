5dbc353b7e8f252cbbaa7954e589b369a84e599d
Hanif <taufiqhanifts01@gmail.com>
2026-01-30 10:04:07 +0700

    004

---
+++ 004.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$nama = "janginam";
+$usia = "55";
+$keluhan = "Glaucoma";
+echo "Nama Pasien: ". $nama . "<br>";
+echo "Usia Pasien: ". $usia . "<br>";
+echo "Keluhan Pasien: ". $keluhan . "<br>";
+?>
+</body>
+</html>
