80602eca5432160b606b93c3e084182c356bf915
Hanif <taufiqhanifts01@gmail.com>
2026-01-31 23:09:07 +0700

    005

---
+++ 005.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$a=12;
+$b=24;
+$c=36;
+$av=3;
+echo "Total dari abc adalah : ". ($a+$b+$c) ."<br />";
+echo "Hasil c dikurang b adalah : ". ($c-$b) ."<br />";
+echo "Hasil b kuadrat adalah : ". ($b*$b) ."<br />";
+echo "Rata-rata dari abc adalah : ". ($a+$b+$c/$av) ."<br />";
+echo "Sisa c dibagi b adalah : ". ($c%$b) ."<br />";
+?>
+</body>
+</html>
